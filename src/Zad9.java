d4a559aea767617b5c3766a34bf8ea19c38875e1
Mateusz Ptak <mateusz.ptak2612@gmail.com>
2025-05-28 21:03:02 +0200

    Zad 9

--- src/Zad9.java
+++ src/Zad9.java
@@ -1,36 +1,45 @@
-// wlasne wyjatki uzywamy gdy chce reprezentowac unikalne bledy ktore nie sa ujete
-// standardowymi wyjatkami lub kiedy chce dodac wiecej kontekstu do bledu niz ten standardowy
+/*
+ ---------  wlasny wyjatek warto pisac gdy:    -------------
 
-//          class BrakSrodkowException extends Exception {
-//              public BrakSrodkowException(String komunikat) {
-//                  super(komunikat);
-//              }
-//}
+ - chce opisac charakterystyczne bledy dla aplikacji ktore nie sa uwzglednione w standardowych wyjatkach jezyka programowania
+ - gdy chce grupowac bledy w jedna kategorie co ulatwia ich obsluge oraz organizacje kody
+ - gdy chce dodac dodatkowe informacje diagnostyczne np. kody bledow, lokalizacje problemu itd ktore pomagaja w szybszym zidentyfikowaniu go
+ - poprawic testy aplikacji umozliwiajac latwiejsza symulacje pewnych sytuacji
 
-// Kiedy warto rzucać wyjątek - throw?
-//Rzucamy wyjątek wtedy, gdy coś poszło nie tak i to nie ta metoda ma to naprawić, tylko ktoś wyżej (np. inna klasa,
-// użytkownik).
-// przyklad
-//                  if (kwota > saldo) {
-//                      throw new BrakSrodkowException("Nie masz wystarczającej ilości pieniędzy.");
-//                  }
+                Przyklad kodu:
 
-
-//Kiedy warto przekazać wyjątek dalej - throws?
-//Gdy metoda może się wywalić, ale to nie jej problem, tylko ktoś wyżej powinien się tym zająć.
-// Przykład:
-//          public void wyplac(double kwota) throws BrakSrodkowException {
-//          rzucamy wyjątek, ale nie obsługujemy go tutaj
-//          }
+ public class InvalidInputException extends Exception {
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
 
 
-// Kiedy warto obsłużyć wyjątek - try - catch?
-// Używamy kiedy wiemy, co zrobić, jeśli coś się nie uda (np. wypisać komunikat, użyć wartości domyślnej),
-// nie chcemy, żeby program się wywalił, chcemy coś posprzątać np. zamknąć plik, połączenie
-//
-//        Przykład:
-//          try {
-//                   konto.wyplac(500);
-//          } catch (BrakSrodkowException e) {
-//                   System.out.println("Błąd: " + e.getMessage());
-//                   }
+
+ ------- Kiedy warto rzucic lub przekazac dalej wyjatek? -------------
+
+ - Rzucamy wyjatek gdy wystapil problem z kontynuowaniem dzialania metody/funkcji
+ - Przekazuje gdy nie ma mozliwosci osbluzenia go na danym poziomie ale wyzszy poziom aplikacji moze podjac odpowiednie dzialania np. dać znać uzytkownikowi, zalogowac jakis blad etc
+ - rzucam wyjatek w sytuacjiach ktore wskazuja na blad logiczny w plikacji, nieoczekiwane warunki, a nie w sytuacjach przewidywalnych takich jak np. bledne wejsciowe ktore powinny byc walidowane wczesniej
+
+ przyklad?
+
+ Jezeli funkcja przetwarzajaca dane otrzyma ich niepoprawny format moze rzucic wyjatkiem ktory bedzie przechwycony na wyzszym poziomie apki ktory zostanie wyswietlony uzytkownikowi
+
+
+ ------- Kiedy obsluzyc wyjatek? -------
+
+ - gdy chce zapobiec przerwaniu dzialania programu i zapewnic jego stabilnosc poprzez odpowiednia reakcje na blad
+ - w celu logowania bledow i monitorowania aplikacji aby ulatwic pozniejsze debugowanie i analize tego problemu
+ - w miejscu gdzie jest mi znany kontekst i moge poprawienia zareagowac na ten blad np przy odczycie pliku gdy plik nie istnieje to moge wyswietlic komunikat o tym i zakonczyc dana operacje bez wylaczania sie programu
+
+ przyklad:
+
+ try {
+    processor.process(null);
+} catch (InvalidInputException e) {
+    System.out.println("Niepoprawne dane wejściowe: " + e.getMessage());
+    // np. poprosić użytkownika o ponowne wprowadzenie danych
+}
+
+*/
